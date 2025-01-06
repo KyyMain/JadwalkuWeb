2744442c5df3c7713693dfca7c4258f773538f6c
Eky Fikri Yamansyah <149387378+KyyMain@users.noreply.github.com>
2025-01-06 19:29:45 +0700

    Delete docs directory

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('themeToggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-
-fetch('fetch_tasks.php')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('tasks').innerHTML = data;
-    });
